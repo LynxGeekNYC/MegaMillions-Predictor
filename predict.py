ab4ddf87ef62a353ab06021dc69509f44cccac55
LynxGeekNYC <45396441+LynxGeekNYC@users.noreply.github.com>
2023-01-11 11:25:14 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,21 +4,14 @@ import random
 # Read the CSV file
 df = pd.read_csv("lottery_results.csv")
 
-# Get the most common numbers for the first 5 spots
-common_nums = df.iloc[:, :5].mode().values[0]
+# Get the most common numbers for all spots
+common_nums = df.mode().values[0]
 
-# Get the most common number for the 6th spot
-common_num = df.iloc[:, 5].mode().values[0]
+# Generate a list of remaining numbers for all spots
+remaining_nums = [i for i in range(1,71) if i not in common_nums[:5]] + [i for i in range(1,26) if i not in common_nums[5:]]
 
-# Generate a list of remaining numbers for 6th spot
-remaining_nums = [i for i in range(1,26) if i not in common_num]
-
-# Choose a random number from the remaining numbers for 6th spot
-next_num = random.choice(remaining_nums)
-
-# Append the next number to the common numbers
-common_nums = list(common_nums)
-common_nums.append(next_num)
+# Choose random numbers from the remaining numbers
+next_nums = random.sample(remaining_nums, 6)
 
 # Print the predicted numbers
-print("Predicted numbers:", common_nums)
+print("Predicted numbers:", next_nums)
